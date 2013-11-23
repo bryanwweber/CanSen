260beeaae9daeadb3319b895edc5328504e779b2
Bryan W. Weber <bryan.w.weber@gmail.com>
2013-11-23 08:57:31 -0500

    Create working constant volume reactor test

--- cansen.py
+++ cansen.py
@@ -1,3 +1,13 @@
 #! /usr/bin/python3
-
-print('Test')
+import cantera as ct
+gas = ct.Solution('mech.cti')
+gas.TPX = 1000,101325,'H2:2,O2:1,N2:3.76'
+reac = ct.Reactor(gas)
+netw = ct.ReactorNet([reac])
+tend = 10
+time = 0
+while time < tend:
+    time = netw.step(tend)
+    print(time,reac.T,reac.thermo.P)
+    if reac.T > 1400:
+        break
