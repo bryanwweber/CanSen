43fccf33bb9cc84bd5ceeb2efb1311f190d5ee2c
Bryan W. Weber <bryan.w.weber@gmail.com>
2013-11-24 17:44:17 -0500

    Revert "Edit setup.py to add author. Add CHANGES.txt"
    This reverts commit 2d8ed94a302fcf7e84022665a1323de13a9e2b80.

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from distutils.core import setup
 setup(
     name='CanSen',
     version='0.0.1',
-    author='Bryan W. Weber',
-    author_email='bryan.w.weber@gmail.com',
     packages=['canset',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
