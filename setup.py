5fab2e86280ea057767c90747a74c98796c43153
Bryan W. Weber <bryan.w.weber@gmail.com>
2013-11-24 17:26:24 -0500

    Change to distribute folder structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='CanSen',
+    version='0.0.1',
+    packages=['canset',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
