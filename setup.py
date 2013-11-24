cdb4242022b3cb34f34bdbdd025f114465c2bfa0
Bryan W. Weber <bryan.w.weber@gmail.com>
2013-11-24 17:44:40 -0500

    Revert "Change to distribute folder structure"
    This reverts commit 5fab2e86280ea057767c90747a74c98796c43153.

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='CanSen',
-    version='0.0.1',
-    packages=['canset',],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
-)
