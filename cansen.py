d22cc48b9c8c2255fc668fdeabe581ef215d0583
Bryan W. Weber <bryan.w.weber@gmail.com>
2013-11-24 17:05:57 -0500

    Add cli_parser. Set up sending to problem fxns
    Problem functions do not exist yet

--- cansen.py
+++ cansen.py
@@ -1,6 +1,5 @@
 #! /usr/bin/python3
 def read_input_file(inputFilename):
-    print("Got to input file")
     reactants = {}
     with open(inputFilename) as inputFile:
         for line in inputFile:
@@ -25,51 +24,92 @@ def read_input_file(inputFilename):
             else:
                 print('Keyword not found',line)
                 sys.exit(1)
-    return problemType,temperature,pressure,reactants,endTime,tempLimit
+    return problemType,temperature,pressure,reactants,endTime,tempLimit,
 
-def main(argv):
+def cli_parser(argv):
     import getopt
-    inputFilename = 'input.inp'
-    outputFilename = 'output.out'
-    mechFilename = 'chem.inp'
-    saveFilename = 'save.pkl'
     help = "Haven't written the help yet, sorry!"
     try:
-        opts, args = getopt.getopt(argv, "hi:o:c:x:",
-                                   ["help", "input=", "output=", "mech=", 
-                                   "save="])
+        opts, args = getopt.getopt(argv, "hi:o:c:d:x:",
+                                   ["help","convert"])
+        options = {}
+        for o,a in opts:
+            options[o] = a
+        
+        if args:
+            raise getopt.GetOptError('Unknown command line option' + 
+                                     repr(' '.join(args))
+                                    )
     except getopt.GetOptError as e:
         print('You did not enter an option properly.')
         print(e)
         print(help)
         sys.exit(1)
-    for opt, arg in opts:
-        if opt in {"-h","--help"}:
-            print(help)
-            sys.exit(0)
-        elif opt in {"-i","--input"}:
-            inputFilename = arg
-        elif opt in {"-o","--output"}:
-            outputFilename = arg
-        elif opt in {"-c","--mech"}:
-            mechFilename = arg
-        elif opt in {"-x","--save"}:
-            saveFilename = arg
+    if '-h' in options or '--help' in options:
+        print(help)
+        sys.exit(0)
+        
+    if '-i' in options:
+        inputFilename = options['-i']
+    else:
+        print('Error: The input file must be specified')
+        sys.exit(1)
+        
+    if '-o' in options:
+        outputFilename = options['-o']
+    else:
+        outputFilename = 'output.out'
+        
+    if '-c' in options:
+        mechFilename = options['-c']
+    else:
+        mechFilename = 'chem.cti'
+    
+    if '-x' in options:
+        saveFilename = options['-x']
+    else:
+        saveFilename = 'save.pkl'
+    
+    if '-d' in options:
+        thermoFilename = options['-d']
+    else:
+        thermoFilename = None
+     
+    convert = '--convert' in options
+    
+    return inputFilename,outputFilename,mechFilename,saveFilename,thermoFilename,convert,
+    
+    
+def main(argv):
+    import os
+    
+    (inputFilename,outputFilename,mechFilename,
+     saveFilename,thermoFilename,convert,) = cli_parser(argv)
+     
     if mechFilename.endswith('.inp'):
         from cantera import ck2cti
-        arg = '--input='+mechFilename
-        ck2cti.main([arg])
+        arg = list('--input='+mechFilename)
+        if thermoFilename is not None:
+            if os.path.isfile(thermoFilename):
+                arg.append('--thermo='+thermoFilename)
+            else:
+                print('Error: Specify proper thermo file')
+        ck2cti.main(arg)
         mechFilename = mechFilename[:-4]+'.cti'
         print(mechFilename)
+    
+    if convert:
+        sys.exit(0)
+        
     ret = read_input_file(inputFilename)
     problemType = ret[0]
-    temperature = ret[1]
-    pressure = ret[2]
-    reactants = ret[3]
-    endTime = ret[4]
-    tempLimit = ret[5]
-    if len(ret) > 6:
-        extraArgs = ret[6:]
+        
+    if problemType == 1:
+        constant_volume_reactor(ret[1:])
+    elif problemType == 2:
+        constant_pressure_reactor(ret[1:])
+    else:
+        print('Error: Unknown Problem Type')
     
 if __name__ == "__main__":
     import sys
