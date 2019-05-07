fbc40be0b4f5b2fbcb370bd35bb19946556e2a4e
Bryan W. Weber <bryan.w.weber@gmail.com>
2019-05-07 17:47:47 -0400

    Run Black formatter on the test code

--- tests/test_cansen.py
+++ tests/test_cansen.py
@@ -5,12 +5,16 @@ from cansen.cansen import main
 
 def test_one_complete_simulation(datadir, capsys):
     """Run an integration/regression test of one simulation."""
-    input_file = datadir / 'test_one_complete_simulation.inp'
-    chem_file = datadir / 'gri30.xml'
-    save_file = datadir / 'save.hdf'
-    output_file = datadir / 'output.out'
-    main(input_filename=str(input_file), mech_filename=str(chem_file), save_filename=str(save_file),
-         output_filename=str(output_file))
+    input_file = datadir / "test_one_complete_simulation.inp"
+    chem_file = datadir / "gri30.xml"
+    save_file = datadir / "save.hdf"
+    output_file = datadir / "output.out"
+    main(
+        input_filename=str(input_file),
+        mech_filename=str(chem_file),
+        save_filename=str(save_file),
+        output_filename=str(output_file),
+    )
     assert output_file.is_file()
     captured = capsys.readouterr()
     assert output_file.read_text() == captured.out
@@ -18,11 +22,16 @@ def test_one_complete_simulation(datadir, capsys):
 
 def test_multi_simulation(datadir):
     """Run an integration/regression test of multiple simulations."""
-    input_file = datadir / 'test_multi_simulation.inp'
-    chem_file = datadir / 'gri30.xml'
-    chem_file = datadir / 'gri30.xml'
-    save_file = datadir / 'save.hdf'
-    output_file = datadir / 'output.out'
-    main(input_filename=str(input_file), mech_filename=str(chem_file), save_filename=str(save_file),
-         output_filename=str(output_file), multi=1)
+    input_file = datadir / "test_multi_simulation.inp"
+    chem_file = datadir / "gri30.xml"
+    chem_file = datadir / "gri30.xml"
+    save_file = datadir / "save.hdf"
+    output_file = datadir / "output.out"
+    main(
+        input_filename=str(input_file),
+        mech_filename=str(chem_file),
+        save_filename=str(save_file),
+        output_filename=str(output_file),
+        multi=1,
+    )
     assert output_file.is_file()
--- tests/test_main.py
+++ tests/test_main.py
@@ -16,65 +16,70 @@ def test_no_args():
         main([])
     assert str(e.value) == "1"
 
+
 def test_version_output(capsys):
     """Test that the version output is printed properly."""
-    loc = Path(__file__).parent.parent.joinpath('cansen')
+    loc = Path(__file__).parent.parent.joinpath("cansen")
 
     with pytest.raises(SystemExit) as e:
-        main(['-V'])
-    assert str(e.value) == '0'
+        main(["-V"])
+    assert str(e.value) == "0"
     captured = capsys.readouterr()
     assert captured.out == f"CanSen {__version__} from {loc} ()\n"
 
     with pytest.raises(SystemExit) as e:
-        main(['--version'])
-    assert str(e.value) == '0'
+        main(["--version"])
+    assert str(e.value) == "0"
     captured = capsys.readouterr()
     assert captured.out == f"CanSen {__version__} from {loc} ()\n"
+
 
 def test_convert_mech_w_thermo(datadir):
     """Test conversion when thermo is in the input file.
 
     This case is the single argument to ``convert_mech``.
     """
-    input_file = datadir / 'test-w-thermo.inp'
-    blessed_file = datadir / 'test-blessed.cti'
+    input_file = datadir / "test-w-thermo.inp"
+    blessed_file = datadir / "test-blessed.cti"
     os.chdir(datadir)
-    output_file = datadir / 'test-w-thermo.cti'
+    output_file = datadir / "test-w-thermo.cti"
     with pytest.raises(SystemExit) as e:
-        main(['--convert', '--chem', str(input_file)])
-    assert str(e.value) == '0'
+        main(["--convert", "--chem", str(input_file)])
+    assert str(e.value) == "0"
     assert output_file.read_text() == blessed_file.read_text()
+
 
 def test_convert_mech_wo_thermo(datadir):
     """Test conversion when thermo is in a separate file.
 
     This is the two argument case for ``convert_mech``.
     """
-    input_file = datadir / 'test-wo-thermo.inp'
-    dat_file = datadir / 'test-wo-thermo.dat'
-    blessed_file = datadir / 'test-blessed.cti'
+    input_file = datadir / "test-wo-thermo.inp"
+    dat_file = datadir / "test-wo-thermo.dat"
+    blessed_file = datadir / "test-blessed.cti"
     os.chdir(datadir)
-    output_file = datadir / 'test-wo-thermo.cti'
+    output_file = datadir / "test-wo-thermo.cti"
     with pytest.raises(SystemExit) as e:
-        main(['--convert', '--chem', str(input_file), '--thermo', str(dat_file)])
-    assert str(e.value) == '0'
+        main(["--convert", "--chem", str(input_file), "--thermo", str(dat_file)])
+    assert str(e.value) == "0"
     assert output_file.read_text() == blessed_file.read_text()
+
 
 def test_convert_mech_missing_chem_file():
     """Test that a missing mechanism file raises a FileNotFoundError."""
-    input_file = 'this-file-does-not-exist.inp'
+    input_file = "this-file-does-not-exist.inp"
     with pytest.raises(FileNotFoundError) as e:
-        main(['--convert', '--chem', input_file])
+        main(["--convert", "--chem", input_file])
 
     assert str(e.value) == f'The chemistry file "{input_file}" could not be found'
 
+
 def test_convert_mech_missing_thermo_file(datadir):
     """Test that a missing mechanism file raises a FileNotFoundError."""
-    input_file = datadir / 'test-wo-thermo.inp'
-    dat_file = datadir / 'this-file-does-not-exist.dat'
+    input_file = datadir / "test-wo-thermo.inp"
+    dat_file = datadir / "this-file-does-not-exist.dat"
     os.chdir(datadir)
     with pytest.raises(FileNotFoundError) as e:
-        main(['--convert', '--chem', str(input_file), '--thermo', str(dat_file)])
+        main(["--convert", "--chem", str(input_file), "--thermo", str(dat_file)])
 
     assert str(e.value) == f'The thermodynamic database "{dat_file}" could not be found'
